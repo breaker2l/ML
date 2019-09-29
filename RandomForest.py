dd334f670359686efd1b228b4c3518e253a154dd
breaker2l <34549956+breaker2l@users.noreply.github.com>
2019-09-29 06:38:12 -0700

    Create RandomForest.py

---
+++ RandomForest.py
@@ -0,0 +1,11 @@
+#Sonar Dataset
+Download : https://goo.gl/FXFWwF
+Top Results: https://goo.gl/3wNAjv
+
+#Random Forest Algorithm on sonar dataset
+from random import seed 
+from random import randrange
+from csv import reader
+from math import sqrt
+
+
