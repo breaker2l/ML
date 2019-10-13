8f29bffb8680e91227e0aa5dafb18889a36adf0a
breaker2l <34549956+breaker2l@users.noreply.github.com>
2019-10-13 05:42:03 -0700

    Update LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -52,4 +52,48 @@ def evaluate_algorithm(dataset, algorithm,split, *args):
     rmse = rmse_metric(actual,predicted)
     return rmse
     
+#Calculate the mean value of a list of numbers
+def mean(values):
+   return sum(values) / float(len(values))
+
+#calculate covariance between x and y
+def covariance(x, mean_x,y,mean_y):
+   covar = 0.0
+   for i in range(len(x)):
+      covar += (x[i] - mean_x) * (y[i] - mean_y)
+   return covar
+
+#Calculate the variance of a list of numbers
+def variance(values, mean):
+   return sum([x-mean)**2 for x in values])
+
+#Calculate coefficients
+def coefficients(dataset):
+   x = [row[0] for row in dataset]
+   y = [row[1] for row in dataset]
+   x_mean, y_mean = mean(x), mean(y)
+   b1 = covariance(x,x_mean,y,y_mean)/ variance(x,x_mean)
+   b0 = y_mean -b1 * x_mean
+   return [b0, b1]
+
+#Simple linear regression algorithm
+def simple_linear_regression(train, test):
+   predictions = list()
+   b0,b1 = coefficients(train)
+   for row in test:
+      yhat = b0 + b1 *row[0]
+      predictions.append(yhat)
+   return predictions
+
+#Simple linear regression on insurance dataset
+seed(1)
+#load and prepare data
+filename = 'insurance.csv'
+dataset = load_csv('filename')
+for i in range(len(dataset[0])):
+   str_column_to_float(dataset, i)
+#evaluate algorithm
+split = 0.6
+rmse = evaluate_algorithm(dataset, simple_linear_regression, split)
+print('RMSE: %3f' % (rmse))
     
